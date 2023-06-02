6fc10ca7d4ef49f000167731a3d4ed3d5c4bc817
RPF35 <126990642+RPF@users.noreply.github.com>
2023-06-01 20:05:48 -0400

    update files

--- Employee_SQL/Employee SQL.sql
+++ Employee_SQL/Employee SQL.sql
@@ -12,30 +12,30 @@ CREATE TABLE IF NOT EXISTS employees(
 CREATE TABLE IF NOT EXISTS dept_emp(
 	emp_no INT NOT NULL, 
 	dept_no VARCHAR(10) NOT NULL,
-	PRIMARY KEY (emp_no,dept_no));
+	CONSTRAINT PK_de PRIMARY KEY (emp_no,dept_no));
 
-	
+
 CREATE TABLE IF NOT EXISTS dept(
 	dept_no VARCHAR(10) PRIMARY KEY NOT NULL,
 	dept_name VARCHAR(200) NOT NULL);
-	
+
+
 CREATE TABLE IF NOT EXISTS dept_manager(
 	dept_no VARCHAR(10) NOT NULL,
 	emp_no INT NOT NULL,
-	PRIMARY KEY (emp_no));	
-	
-CREATE TABLE IF NOT EXISTS salaries(
-	salary_id INT PRIMARY KEY NOT NULL,
-	emp_no INT NOT NULL,
-	salary INT NOT NULL);
+	CONSTRAINT PK_dm PRIMARY KEY (dept_no,emp_no));	
 
-		
+
+CREATE TABLE IF NOT EXISTS salaries(
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	CONSTRAINT PK_s PRIMARY KEY (emp_no,salary));
+
+	
 CREATE TABLE IF NOT EXISTS titles(
-	emp_title_id VARCHAR(10) PRIMARY KEY NOT NULL,
+	emp_title_id VARCHAR(25) PRIMARY KEY NOT NULL,
 	title VARCHAR(200) NOT NULL);
 
-ALTER TABLE titles
-	ALTER COLUMN emp_title_id TYPE VARCHAR(25);
 	
 --adding foreign key contraints
 ALTER TABLE employees
@@ -43,17 +43,6 @@ ALTER TABLE employees
 
 ALTER TABLE dept_emp
 	ADD FOREIGN KEY (emp_no) REFERENCES employees(emp_no);
-
-ALTER TABLE dept_emp
-	ADD CONSTRAINT unique_dept_emp_key UNIQUE (emp_no, dept_no);
-
-ALTER TABLE dept_emp DROP CONSTRAINT unique_dept_emp_key;
-
-ALTER TABLE dept
-	ADD CONSTRAINT unique_dept_emp_key UNIQUE (dept_no, dept_name);
-
-ALTER TABLE dept
-	ADD FOREIGN KEY (dept_no) REFERENCES dept_emp(dept_no);
 
 ALTER TABLE dept_manager
 	ADD FOREIGN KEY (dept_no) REFERENCES dept(dept_no);
